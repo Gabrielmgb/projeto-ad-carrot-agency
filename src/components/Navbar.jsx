60c89ffca4445e27a9809bc2ec7ad7b7d2fb3858
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-05-02 17:55:45 -0300

    Team

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 import { FaBars, FaTimes } from "react-icons/fa";
-import {LINKS} from "../constants"
+import { LINKS } from "../constants";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -50,23 +50,36 @@ const Navbar = () => {
               </a>
             </div>
             <div>
-                <button onClick={() => setIsOpen(!isOpen)} type="button" className="inline-flex items-center justify-center bg-emerald-950 p-2 text-orange-50">
-                    <FaBars className="h-6 w-6"/>
-                </button>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="inline-flex items-center justify-center bg-emerald-950 p-2 text-orange-50"
+              >
+                <FaBars className="h-6 w-6" />
+              </button>
             </div>
           </div>
         </div>
       </nav>
       {isOpen && (
         <div className="fixed inset-0 z-20 flex flex-col space-y-0 bg-emerald-950 px-20 pt-20 text-5xl font-bold uppercase text-emerald-100 lg:text-6xl">
-            <button onClick={() => setIsOpen(false)} type="button" className="absolute right-4 top-4 rounded-full bg-emerald-900 p-2 text-orange-50 lg:right-20">
-                <FaTimes className="h-8 w-8"/>
-            </button>
-            {LINKS.map((link, index) => (
-                <a key={index} href={`#${link.id}`} onClick={() => handleLinkClick(e, link.id)} className="transition-colors duration-500 hover:text-orange-500">
-                    {link.name}
-                </a>
-            ))}
+          <button
+            onClick={() => setIsOpen(false)}
+            type="button"
+            className="absolute right-4 top-4 rounded-full bg-emerald-900 p-2 text-orange-50 lg:right-20"
+          >
+            <FaTimes className="h-8 w-8" />
+          </button>
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={`#${link.id}`}
+              onClick={() => handleLinkClick(e, link.id)}
+              className="transition-colors duration-500 hover:text-orange-500"
+            >
+              {link.name}
+            </a>
+          ))}
         </div>
       )}
     </>
