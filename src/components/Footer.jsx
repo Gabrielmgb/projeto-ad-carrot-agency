8d6b4bf36b6c200de0fa064457781de0459b09a7
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-05-12 22:37:17 -0300

    Footer hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center
        justify-center space-y-4 md:space-y-0">
         <div className="mb-4 flex space-x-4">
-            <a href="#"> <FaFacebookF size={20} /></a>
-            <a href="#"> <FaTwitter size={20} /></a>
-            <a href="#"> <FaInstagram size={20} /></a>
-            <a href="#"> <FaLinkedinIn size={20} /></a>
+            <a href="#" className="hover:text-neutral-200 duration-200"> <FaFacebookF size={20} /></a>
+            <a href="#" className="hover:text-neutral-200 duration-200"> <FaTwitter size={20} /></a>
+            <a href="#" className="hover:text-neutral-200 duration-200"> <FaInstagram size={20} /></a>
+            <a href="#" className="hover:text-neutral-200 duration-200"> <FaLinkedinIn size={20} /></a>
         </div>
         <div className="text-sm opacity-50">
             &copy; {new Date().getFullYear()} adCarrot. All rights reserverd
