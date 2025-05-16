fef44f16fb9cf1ea836aa6ab35a7d1b1d841805a
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-05-16 20:12:42 -0300

    hero bg zinc 950

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <section
       className="flex
-     min-h-screen flex-wrap items-center bg-emerald-950 text-orange-50"
+     min-h-screen flex-wrap items-center bg-zinc-950 text-orange-50"
     >
       <div className="w-full p-8 md:w-1/2">
         <div className="mb-4 flex space-x-2">
