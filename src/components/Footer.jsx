aa5bf229e6a05f530278af0c2afd69eed7125e01
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-05-17 19:03:22 -0300

    footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/f
 
 const Footer = () => {
   return (
-    <footer className=" bg-emerald-950 py-6 text-orange-50">
+    <footer className=" bg-blackBrown py-6 text-orange-100">
       <div className="container mx-auto flex flex-col items-center
        justify-center space-y-4 md:space-y-0">
         <div className="mb-4 flex space-x-4">
-            <a href="#" className="hover:text-neutral-200 duration-200"> <FaFacebookF size={20} /></a>
-            <a href="#" className="hover:text-neutral-200 duration-200"> <FaTwitter size={20} /></a>
-            <a href="#" className="hover:text-neutral-200 duration-200"> <FaInstagram size={20} /></a>
-            <a href="#" className="hover:text-neutral-200 duration-200"> <FaLinkedinIn size={20} /></a>
+            <a href="#" className="hover:text-orange duration-200"> <FaFacebookF size={20} /></a>
+            <a href="#" className="hover:text-orange duration-200"> <FaTwitter size={20} /></a>
+            <a href="#" className="hover:text-orange duration-200"> <FaInstagram size={20} /></a>
+            <a href="#" className="hover:text-orange duration-200"> <FaLinkedinIn size={20} /></a>
         </div>
         <div className="text-sm opacity-50">
             &copy; {new Date().getFullYear()} adCarrot. All rights reserverd
