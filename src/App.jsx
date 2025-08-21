fd1b43cd02669aa5a84608787aabaf3117ec5504
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-08-20 23:35:18 -0300

    Organized code from app

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,17 @@ import Testimonials from './components/Testimonials'
 const App = () => {
   return (
     <main className="overflow-y-hidden bg-orange-50 text-blackBrown antialiased">
-        <Navbar/>
-        <Hero/>
-        <About/>
-        <Services/>
-        <ProjectShowcase/>
-        <AdAgencyPackages/>
-        <Team/>
-        <Testimonials/>
-        <Faq/>
-        <ContactForm/>
-        <Footer/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <ProjectShowcase />
+      <AdAgencyPackages />
+      <Team />
+      <Testimonials />
+      <Faq />
+      <ContactForm />
+      <Footer />
     </main>
   )
 }
