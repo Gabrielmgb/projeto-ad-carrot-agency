6053209dbaa047ebd08ead702e3e9c155ef8dab5
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-08-22 23:27:31 -0300

    Organized code from Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,27 +14,27 @@ const Hero = () => {
       <div className="w-full p-8 mt-[40px] md:mt-none md:w-1/2">
         <div className="mb-4 flex space-x-2">
           <motion.img
-            initial= {{ opacity: 0, scale: 0.8 }}
-            animate= {{ opacity: 1, scale: 1 }}
-            transition= {{ duration: 0.8 , delay: 0.1 }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, delay: 0.1 }}
 
             src={profileImg1}
             alt="Team Member 1"
             className="h-12 w-12 rounded-full border-2 border-orange"
           />
           <motion.img
-            initial= {{ opacity: 0, scale: 0.8 }}
-            animate= {{ opacity: 1, scale: 1 }}
-            transition= {{ duration: 0.8 , delay: 0.2 }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
 
             src={profileImg2}
             alt="Team Member 2"
             className="h-12 w-12 rounded-full border-2 border-orange"
           />
           <motion.img
-          initial= {{ opacity: 0, scale: 0.8 }}
-          animate= {{ opacity: 1, scale: 1 }}
-          transition= {{ duration: 0.8 , delay: 0.3 }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, delay: 0.3 }}
 
             src={profileImg3}
             alt="Team Member 3"
@@ -42,31 +42,31 @@ const Hero = () => {
           />
         </div>
         <motion.h1
-        initial= {{ opacity: 0, y: -20 }}
-        animate= {{ opacity: 1, y: 0 }}
-        transition= {{ duration: 0.8 }}
-        
-        className="text-4xl tracking-tighter md:text-5xl lg:text-7xl">
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+
+          className="text-4xl tracking-tighter md:text-5xl lg:text-7xl">
           We create impactful ads that
           <span className=" text-orange-500"> elevate</span> your brand.
         </motion.h1>
-        <motion.button 
-        initial= {{ opacity: 0, y: -20}}
-        animate= {{ opacity: 1, y: 0 }}
-        transition= {{ duration: 1 }}
-        
-        className="mt-8 flex items-center gap-2 rounded-full bg-brown px-4 py-3 hover:bg-lightBrown duration-200 cursor-pointer">
+        <motion.button
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+
+          className="mt-8 flex items-center gap-2 rounded-full bg-brown px-4 py-3 hover:bg-lightBrown duration-200 cursor-pointer">
           <span>know More</span>
           <IoArrowForward />
         </motion.button>
       </div>
       <div className="w-full md:w-1/2 lg:p-8">
         <motion.img
-        initial= {{ opacity: 0, scale: 0.8 }}
-        animate= {{ opacity: 1, scale: 1 }}
-        transition= {{ duration: 0.8 }}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8 }}
 
-        src={heroImg} alt="Ad Agency" className="h-auto w-full" />
+          src={heroImg} alt="Ad Agency" className="h-auto w-full" />
       </div>
     </section>
   );
