c0b5b132bc89066c1c8bf76e59903610ddd7e49b
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-08-25 23:40:20 -0300

    Organized code from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,15 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center
        justify-center space-y-4 md:space-y-0">
         <div className="mb-4 flex space-x-4">
-            <a href="#" className="hover:text-orange duration-200"> <FaFacebookF size={20} /></a>
-            <a href="#" className="hover:text-orange duration-200"> <FaTwitter size={20} /></a>
-            <a href="#" className="hover:text-orange duration-200"> <FaInstagram size={20} /></a>
-            <a href="#" className="hover:text-orange duration-200"> <FaLinkedinIn size={20} /></a>
+          <a href="#" className="hover:text-orange duration-200"> <FaFacebookF size={20} /></a>
+          <a href="#" className="hover:text-orange duration-200"> <FaTwitter size={20} /></a>
+          <a href="#" className="hover:text-orange duration-200"> <FaInstagram size={20} /></a>
+          <a href="#" className="hover:text-orange duration-200"> <FaLinkedinIn size={20} /></a>
         </div>
         <div className="text-sm opacity-50">
-            &copy; {new Date().getFullYear()} adCarrot. All rights reserverd
+          &copy; {new Date().getFullYear()} adCarrot. All rights reserverd
         </div>
-        
+
       </div>
     </footer>
   )
